dae275b486489736be43a7d62735d1de6137fb9f
papaeng89 <47139293+papaeng89@users.noreply.github.com>
2024-03-21 19:59:48 +0100

    Update capture.py
    Change for use ffmpeg than VLC

--- capture.py
+++ capture.py
@@ -1,29 +1,36 @@
 import time
-import vlc
 import datetime
 import os
+import subprocess
+import logging
 import timelapseconfig
-
-#prepare VLC
-i = vlc.Instance("--vout=dummy") # when running from cron, there is no screen attached
-camera = i.media_player_new()
-camera.set_mrl(timelapseconfig.rtsp_url)
-camera.audio_set_volume(0)
-camera.play()
 
 # prepare the output folder
 write_path = timelapseconfig.output_dir + datetime.datetime.today().strftime(timelapseconfig.daily_foldername_date_formatstring)
 if not os.path.exists(write_path):
     os.makedirs(write_path)
 
-print ('waiting for VLC')
 time.sleep(10)
-print ("staring capture")
+print ("Starting capture")
 starttime = datetime.datetime.now().timestamp()
+failed = 0
+succeeded = 0
 while datetime.datetime.now().timestamp() < starttime + timelapseconfig.cronjob_repeat_time:
     cycle_starttime = datetime.datetime.now().timestamp()
     file_name = write_path + "/" + str(int(datetime.datetime.now().timestamp())) + ".png"
     print(file_name)
-    camera.video_take_snapshot(0,file_name,1920,1080)
+    logging.info(f"Capturing image {succeeded + failed + 1}...")
+    res = subprocess.call(
+        f"ffmpeg -y -loglevel fatal -rtsp_transport tcp -i {timelapseconfig.rtsp_url} -frames:v 1 {file_name}",
+        shell=True,
+    )
+    if res == 0:
+        succeeded += 1
+    else:
+        failed += 1
+    end = time.time()    
+    logging.info(f"Succeeded: {succeeded}, failed: {failed}")
+    #camera.video_take_snapshot(0,file_name,1920,1080)
+
     while datetime.datetime.now().timestamp() < cycle_starttime + timelapseconfig.delay_between_images:
         time.sleep(0.1)
